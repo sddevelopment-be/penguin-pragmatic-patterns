60a934d38e9de594151041bba76c3f7a1cd3aef2
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2023-10-25 07:37:11 +0200

    Visuals: add single page display for books
    - Add utility script to generate single pages for book from TOML file
    - Add `ops` directory and README to explain it's purpose
    - Run book-page generation script
    - Update layout of single page book display
    
    Fixes the bug described in github issue tracker item #41.

---
+++ ops/generate_books.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Simple STAX parser implementation for a TOML file containing bibliographical information
+outputDir="content/books"
+book=false;
+title=""
+id=""
+title=""
+draft="false"
+tags="[]"
+complexity="[]"
+
+while read p; do
+  if [ "$p" = "[[book]]" ]; then
+    book=true
+  fi
+  if [ "$p" = "" ]; then
+      outFile="$outputDir/$(echo $id | sed "s/[\" ]//g").md";
+      echo "+++" > $outFile;
+      echo "title = $title" >> $outFile;
+      echo "id = $id" >> $outFile;
+      echo "tags = $tags" >> $outFile;
+      echo "levels = $complexity" >> $outFile;
+      echo "draft = $draft" >> $outFile;
+      echo "+++" >> $outFile;
+      echo "Wrote data to $outFile !";
+
+      book=false
+      title=""
+      id=""
+      draft=false
+      title=""
+      tags="[]"
+      complexity="[]"
+  fi
+
+  if [[ $book ]]; then
+    if [[ "$p" = "id = "* ]]; then
+        id=$(echo "$p" | awk -F"=" '/id/{print $2}');
+    fi
+    if [[ "$p" = "title = "* ]]; then
+        title=$(echo "$p" | awk -F"=" '/title/{print $2}');
+    fi
+    if [[ "$p" = "tags = "* ]]; then
+        tags=$(echo "$p" | awk -F"=" '/tags/{print $2}');
+    fi
+    if [[ "$p" = "levels = "* ]]; then
+        complexity=$(echo "$p" | awk -F"=" '/levels/{print $2}');
+    fi
+    if [[ "$p" = "draft = "* ]]; then
+        draft=$(echo "$p" | awk -F"=" '/draft/{print $2}');
+    fi
+  fi
+done < $1

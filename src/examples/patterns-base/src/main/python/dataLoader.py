55c247d540683f41f89f5070b505c51ce4aa6265
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2024-05-18 11:17:58 +0200

    Add example for Fail Fast practise

--- src/examples/patterns-base/src/main/python/dataLoader.py
+++ src/examples/patterns-base/src/main/python/dataLoader.py
@@ -10,22 +10,31 @@ class HeaderFields(Enum):
     NAME = 'name'
 
 
-def validate_file(file_to_validate):
-    with open(file_to_validate, newline='') as file:
-        header_line = file.readline();
-
-
 def process_file(file_to_process):
     with open(file_to_process, newline='') as file:
+
+        print('|', ' | '.join([field.value for field in HeaderFields]), end=' |\n', file=sys.stdout, flush=False)
+        print('|', ' | '.join(['---' for _ in HeaderFields]), end=' |\n', file=sys.stdout, flush=False)
         data = csv.DictReader(file)
         for i, row in enumerate(data):
             output = []
             for field in HeaderFields:
                 if field.value not in row:
-                    print('Error: Missing field [', field.value, '] in input file at line: ', i+1)
+                    print('Error: Missing field [', field.value, '] in input file at line: ', i + 1)
                     sys.exit(1)
                 output.append(row[field.value])
-            print(output, sep=' ', end='\n', file=sys.stdout, flush=False)
+            print('|', ' | '.join(output), end='|\n', file=sys.stdout, flush=False)
+
+
+def validate_file(file_to_check):
+    is_valid = True
+    with open(file_to_check, newline='') as file:
+        header = next(csv.reader(file))
+        for field in HeaderFields:
+            if field.value not in header:
+                print('Error: Missing field [', field.value, '] in input file [', file_to_check, ']')
+                is_valid = False
+    return is_valid
 
 
 def main():
@@ -34,8 +43,11 @@ def main():
         sys.exit(1)
 
     input_file = sys.argv[1]
-    validate_file(file_to_validate=input_file)
-    process_file(file_to_process=input_file)
+    is_valid = validate_file(file_to_check=input_file)
+    if(not is_valid):
+        print('Error: Invalid input file, SKIPPING [', input_file, ']')
+    else:
+        process_file(file_to_process=input_file)
 
 
 if __name__ == "__main__":

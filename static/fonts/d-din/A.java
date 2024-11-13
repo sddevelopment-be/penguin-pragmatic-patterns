4fef28560fb265623a59274f7842947ec7dfe3e8
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2024-11-13 07:01:23 +0100

    Layout update: overhaul
    * Add new logo
    * Synchronize fonts
    * Update css displays
    * Fix issues with styling

---
+++ static/fonts/d-din/A.java
@@ -0,0 +1,9 @@
+/home/stijnd/Downloads/d-din/D-DIN.otf
+/home/stijnd/Downloads/d-din/D-DIN-Bold.otf
+/home/stijnd/Downloads/d-din/D-DINCondensed.otf
+/home/stijnd/Downloads/d-din/D-DINCondensed-Bold.otf
+/home/stijnd/Downloads/d-din/D-DINExp.otf
+/home/stijnd/Downloads/d-din/D-DINExp-Bold.otf
+/home/stijnd/Downloads/d-din/D-DINExp-Italic.otf
+/home/stijnd/Downloads/d-din/D-DIN-Italic.otf
+/home/stijnd/Downloads/d-din/SIL Open Font License.txt

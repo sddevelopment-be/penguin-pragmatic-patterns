e8e7bca0c827eba2997de67cba0831e6f5ea31a3
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2025-11-14 07:18:32 +0100

    Refactor location of diagrams

--- src/examples/productivity_with_shortcuts/src/main/java/be/swsb/productivity/chapter2/Jumper.java
+++
@@ -1,6 +0,0 @@
-package be.swsb.productivity.chapter2;
-
-public interface Jumper {
-
-    String jump();
-}
--- src/examples/productivity_with_shortcuts/src/main/java/be/swsb/productivity/chapter6/transformers/DecepticonEnum.java
+++
@@ -1,8 +0,0 @@
-package be.swsb.productivity.chapter6.transformers;
-
-public enum DecepticonEnum {
-    JET,
-    INSECTICON,
-    GUN,
-    DUOCON
-}
